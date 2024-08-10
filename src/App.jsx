fb316dda89c278c0a352f8d7319686d9f65c6f78
freddyfavour <alfredfavour76@gmail.com>
2024-08-10 20:14:43 +0100

    Added a location functionality to SearchParams.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
+
+export default App;
