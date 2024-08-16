54ba6ce53c45872d52339eedaa7c2fff45fc6923
freddyfavour <alfredfavour76@gmail.com>
2024-08-16 17:36:03 +0100

    Added React Router Dom Created Details.jsx for the detailed page for pets as well as a Results.jsx and useBreedList.jsx Improved Pet.jsx Added More State to SearchParams and an async funtion for requestingpets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
